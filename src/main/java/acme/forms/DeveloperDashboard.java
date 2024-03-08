d40f9159f009643298dd717d95fa58b8a4881a85
sheche1 <92683001+sheche1@users.noreply.github.com>
2024-03-08 18:34:55 +0100

    corrected errors

--- src/main/java/acme/forms/DeveloperDashboard.java
+++ src/main/java/acme/forms/DeveloperDashboard.java
@@ -3,51 +3,32 @@ package acme.forms;
 
 import java.util.Date;
 
-import javax.persistence.Entity;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import acme.client.data.AbstractEntity;
+import acme.client.data.AbstractForm;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
 @Getter
 @Setter
-public class DeveloperDashboard extends AbstractEntity {
+public class DeveloperDashboard extends AbstractForm {
 
 	// Serialisation identifier
 	private static final long	serialVersionUID	= 1L;
 
 	// Attributes
-	@NotNull
-	private Integer				totalTrainingModules;
+	Integer						totalTrainingModules;
 
-	@NotNull
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date				trainingModulesUpdateMoment;
+	Date						trainingModulesUpdateMoment;
 
-	@NotNull
-	private Integer				totalTrainingSessions;
+	Integer						totalTrainingSessions;
 
-	@NotNull
-	@Size(max = 255)
-	@NotBlank
-	private String				trainingSessionsLink;
+	String						trainingSessionsLink;
 
-	@NotNull
-	private Double				averageTrainingModuleTime;
+	Double						averageTrainingModuleTime;
 
-	@NotNull
-	private Double				deviationTrainingModuleTime;
+	Double						deviationTrainingModuleTime;
 
-	@NotNull
-	private Integer				minimumTrainingModuleTime;
+	Integer						minimumTrainingModuleTime;
 
-	@NotNull
-	private Integer				maximumTrainingModuleTime;
+	Integer						maximumTrainingModuleTime;
 
 }
