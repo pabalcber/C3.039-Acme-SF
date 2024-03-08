6ec4e4a2c90c52ed1bd1add67346987eb001ddcb
maravimaq <maravimaq@alum.us.es>
2024-03-08 11:45:21 +0100

    Añadida relación entre proyectos y contratos y datos en el csv

--- src/main/java/acme/entities/contracts/Contract.java
+++ src/main/java/acme/entities/contracts/Contract.java
@@ -18,6 +18,7 @@ import org.hibernate.validator.constraints.Length;
 
 import acme.client.data.AbstractEntity;
 import acme.client.data.datatypes.Money;
+import acme.entities.projects.Project;
 import acme.roles.clients.Client;
 import lombok.Getter;
 import lombok.Setter;
@@ -33,7 +34,7 @@ public class Contract extends AbstractEntity {
 	// Attributes -------------------------------------------------------------
 	@Column(unique = true)
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}", message = "El código debe seguir el patrón '[A-Z]{1,3}-[0-9]{3}'")
+	@Pattern(regexp = "^[A-Z]{1,3}-[0-9]{3}$", message = "El código debe seguir el patrón '[A-Z]{1,3}-[0-9]{3}'")
 	private String				code;
 
 	@NotNull
@@ -66,4 +67,9 @@ public class Contract extends AbstractEntity {
 	@Valid
 	@ManyToOne(optional = false)
 	private Client				client;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Project				project;
 }
