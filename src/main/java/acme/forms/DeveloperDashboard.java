77fe5b7416ced21a08a2ec7abae91e8257b1be76
sheche1 <92683001+sheche1@users.noreply.github.com>
2024-03-08 18:29:56 +0100

    fix

---
+++ src/main/java/acme/forms/DeveloperDashboard.java
@@ -0,0 +1,53 @@
+
+package acme.forms;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class DeveloperDashboard extends AbstractEntity {
+
+	// Serialisation identifier
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes
+	@NotNull
+	private Integer				totalTrainingModules;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				trainingModulesUpdateMoment;
+
+	@NotNull
+	private Integer				totalTrainingSessions;
+
+	@NotNull
+	@Size(max = 255)
+	@NotBlank
+	private String				trainingSessionsLink;
+
+	@NotNull
+	private Double				averageTrainingModuleTime;
+
+	@NotNull
+	private Double				deviationTrainingModuleTime;
+
+	@NotNull
+	private Integer				minimumTrainingModuleTime;
+
+	@NotNull
+	private Integer				maximumTrainingModuleTime;
+
+}
