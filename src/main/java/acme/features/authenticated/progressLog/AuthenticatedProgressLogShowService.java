b89ca7b24f674295204e1f6ad33c4528a0e941b2
maravimaq <maravimaq@alum.us.es>
2024-04-07 13:00:32 +0200

    List Progress Logs and Show details all principals

---
+++ src/main/java/acme/features/authenticated/progressLog/AuthenticatedProgressLogShowService.java
@@ -0,0 +1,50 @@
+
+package acme.features.authenticated.progressLog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.data.accounts.Authenticated;
+import acme.client.data.models.Dataset;
+import acme.client.services.AbstractService;
+import acme.entities.contracts.ProgressLog;
+
+@Service
+public class AuthenticatedProgressLogShowService extends AbstractService<Authenticated, ProgressLog> {
+	// Internal State ---------------------------------------------------------------------------------------------------------------------------------------
+
+	@Autowired
+	private AuthenticatedProgressLogRepository repository;
+
+
+	// AbstractService interface ----------------------------------------------------------------------------------------------------------------------------
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		ProgressLog object;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		object = this.repository.findOneProgressLogById(id);
+
+		super.getBuffer().addData(object);
+	}
+
+	@Override
+	public void unbind(final ProgressLog object) {
+		assert object != null;
+
+		Dataset dataset;
+
+		dataset = super.unbind(object, "recordId", "completeness", "comment", "registrationMoment", "responsiblePerson", "contract");
+		dataset.put("masterId", object.getContract().getId());
+		dataset.put("draftMode", object.getContract().isDraftMode());
+
+		super.getResponse().addData(dataset);
+	}
+
+}
--- src/main/webapp/WEB-INF/views/authenticated/contract/form.jsp
+++ src/main/webapp/WEB-INF/views/authenticated/contract/form.jsp
@@ -9,4 +9,11 @@
 	<acme:input-textbox code="authenticated.contract.form.label.customerName" path="customerName"/>
 	<acme:input-money code="authenticated.contract.form.label.budget" path="budget"/>
 	<acme:input-textbox code="authenticated.contract.form.label.project" path="project"/>
+	
+	<jstl:if test="${_command == 'show'}">
+	<acme:button code="authenticated.contract.form.button.progress-log" action="/authenticated/progress-log/list?masterId=${id}"/>
+</jstl:if>
+	
+	
+	
 </acme:form>
