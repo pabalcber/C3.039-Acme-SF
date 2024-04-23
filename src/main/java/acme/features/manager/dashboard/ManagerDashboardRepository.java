fa4eec7414c0d8870eb29e96df9864a5d7c00fe4
pabalcber <pabloalcantarabernal@gmail.com>
2024-04-23 18:39:35 +0200

    Fix: Draftmode issues with project/userStory/ManagerDashboard

--- src/main/java/acme/features/manager/dashboard/ManagerDashboardRepository.java
+++ src/main/java/acme/features/manager/dashboard/ManagerDashboardRepository.java
@@ -10,64 +10,31 @@ import acme.entities.userStories.Priority;
 @Repository
 public interface ManagerDashboardRepository extends AbstractRepository {
 
-	@Query("select count(u) from UserStory u where u.priority = :priority and u.project.manager.id = :managerId")
+	@Query("select count(u) from UserStory u where u.priority = :priority and u.project.manager.id = :managerId and u.draftMode=false")
 	int priorityUserStories(int managerId, Priority priority);
 
-	@Query("select avg(u.estimatedCost) from UserStory u where u.project.manager.id = :managerId")
+	@Query("select avg(u.estimatedCost) from UserStory u where u.project.manager.id = :managerId and u.draftMode=false")
 	Double averageEstimatedCost(int managerId);
 
-	@Query("select stddev(u.estimatedCost) from UserStory u where u.project.manager.id = :managerId")
+	@Query("select stddev(u.estimatedCost) from UserStory u where u.project.manager.id = :managerId and u.draftMode=false")
 	Double deviationEstimatedCost(int managerId);
 
-	@Query("select min(u.estimatedCost) from UserStory u where u.project.manager.id = :managerId")
+	@Query("select min(u.estimatedCost) from UserStory u where u.project.manager.id = :managerId and u.draftMode=false")
 	Double minimumEstimatedCost(int managerId);
 
-	@Query("select max(u.estimatedCost) from UserStory u where u.project.manager.id = :managerId")
+	@Query("select max(u.estimatedCost) from UserStory u where u.project.manager.id = :managerId and u.draftMode=false")
 	Double maximumEstimatedCost(int managerId);
 
-	@Query("select avg(p.cost.amount) from Project p where p.cost.currency = :currency and p.manager.id = :managerId")
+	@Query("select avg(p.cost.amount) from Project p where p.cost.currency = :currency and p.manager.id = :managerId and p.draftMode=false")
 	Double averageProjectCosts(String currency, int managerId);
 
-	//	@Query("select avg(p.cost.amount) from Project p where p.cost.currency = 'EUR' and p.manager.id = :managerId")
-	//	Double averageProjectCostsEUR(int managerId);
-	//
-	//	@Query("select avg(p.cost.amount) from Project p where p.cost.currency = 'GBP' and p.manager.id = :managerId")
-	//	Double averageProjectCostsGBP(int managerId);
-	//
-	//	@Query("select avg(p.cost.amount) from Project p where p.cost.currency = 'USD' and p.manager.id = :managerId")
-	//	Double averageProjectCostsUSD(int managerId);
-
-	@Query("select stddev(p.cost.amount) from Project p where p.cost.currency = :currency and p.manager.id = :managerId")
+	@Query("select stddev(p.cost.amount) from Project p where p.cost.currency = :currency and p.manager.id = :managerId and p.draftMode=false")
 	Double deviationProjectCosts(String currency, int managerId);
 
-	//	@Query("select stddev(p.cost.amount) from Project p where p.cost.currency = 'EUR' and p.manager.id = :managerId")
-	//	Double deviationProjectCostsEUR(int managerId);
-	//
-	//	@Query("select stddev(p.cost.amount) from Project p where p.cost.currency = 'GBP' and p.manager.id = :managerId")
-	//	Double deviationProjectCostsGBP(int managerId);
-	//
-	//	@Query("select stddev(p.cost.amount) from Project p where p.cost.currency = 'USD' and p.manager.id = :managerId")
-	//	Double deviationProjectCostsUSD(int managerId);
-
-	@Query("select min(p.cost.amount) from Project p where p.cost.currency = :currency and p.manager.id = :managerId")
+	@Query("select min(p.cost.amount) from Project p where p.cost.currency = :currency and p.manager.id = :managerId and p.draftMode=false")
 	Double minimumProjectCosts(String currency, int managerId);
 
-	//	@Query("select min(p.cost.amount) from Project p where p.cost.currency = :currency and p.manager.id = :managerId")
-	//	Double minimumProjectCostsEUR(int managerId);
-	//
-	//	@Query("select min(p.cost.amount) from Project p where p.cost.currency = 'GBP' and p.manager.id = :managerId")
-	//	Double minimumProjectCostsGBP(int managerId);
-	//
-	//	@Query("select min(p.cost.amount) from Project p where p.cost.currency = 'USD' and p.manager.id = :managerId")
-	//	Double minimumProjectCostsUSD(int managerId);
-
-	@Query("select max(p.cost.amount) from Project p where p.cost.currency = :currency and p.manager.id = :managerId")
+	@Query("select max(p.cost.amount) from Project p where p.cost.currency = :currency and p.manager.id = :managerId and p.draftMode=false")
 	Double maximumProjectCosts(String currency, int managerId);
-
-	//	@Query("select max(p.cost.amount) from Project p where p.cost.currency = 'GBP' and p.manager.id = :managerId")
-	//	Double maximumProjectCostsGBP(int managerId);
-	//
-	//	@Query("select max(p.cost.amount) from Project p where p.cost.currency = 'USD' and p.manager.id = :managerId")
-	//	Double maximumProjectCostsUSD(int managerId);
 
 }
