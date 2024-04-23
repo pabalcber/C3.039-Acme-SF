6e9cb4814f81fc8b4a2b41f4006a8bcbc1b418cc
María Barrancos Márquez <mariabarrancosm@gmail.com>
2024-04-24 01:31:42 +0200

    S5 CodeAudit Show

--- src/main/java/acme/features/auditor/codeAudits/AuditorCodeAuditController.java
+++ src/main/java/acme/features/auditor/codeAudits/AuditorCodeAuditController.java
@@ -16,7 +16,10 @@ public class AuditorCodeAuditController extends AbstractController<Auditor, Code
 	// Internal state ---------------------------------------------------------
 
 	@Autowired
-	private AuditorCodeAuditListService listService;
+	private AuditorCodeAuditListService	listService;
+
+	@Autowired
+	private AuditorCodeAuditShowService	showService;
 
 	// Constructors -----------------------------------------------------------
 
@@ -24,6 +27,7 @@ public class AuditorCodeAuditController extends AbstractController<Auditor, Code
 	@PostConstruct
 	protected void initialise() {
 		super.addBasicCommand("list", this.listService);
+		super.addBasicCommand("show", this.showService);
 
 	}
 }
